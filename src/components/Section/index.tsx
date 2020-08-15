f7edfe4faa479927bc043f96524f01c61e898d27
Rafaela Polizel <rafaela.polizel@pca.com.br>
2020-08-14 22:45:38 -0300

    Added Section component

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import { Container, HeaderWrapper, Header, DropboxLogo, Content} from './styles';
 interface Props {
   variant: 'blue' | 'beige' | 'white' | 'black';
   title: string;
   description: string;
 }
 
-const Section: React.FC<Props> = () => {
-  return <div />;
+const Section: React.FC<Props> = ({variant, title, description}) => {
+  return (
+    <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>Dropbox</span>
+          </h1>
+          <button>Interagir</button>
+        </Header>
+      </HeaderWrapper>
+      <Content>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </Content>
+    </Container>
+  )
 }
 
 export default Section;
